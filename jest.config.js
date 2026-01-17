7153eb023a7a6c83225d63aee728f509d170a2a5
David Novák <mail@davenov.com>
2026-01-17 18:21:35 +0100

    chore(02-04): configure jest for web testing
    - Update jest.config.js with testEnvironment: 'jsdom'
    - Add jest.setup.js importing @testing-library/jest-dom
    - Configure transform for TypeScript files
    - Set testMatch pattern for .test.tsx/.test.ts files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
-module.exports = {
-  preset: 'react-native',
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native(-community)?|react-native-svg)',
-  ],
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  testMatch: ['**/__tests__/**/*.test.tsx', '**/__tests__/**/*.test.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
+
+module.exports = config;
